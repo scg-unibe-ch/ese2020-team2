d47ea5a676c5c46fb50fa32e452f6a3a70701226
Yves Zimmermann <yves.zimmermann1@students.unibe.ch>
2020-10-08 08:12:40 +0200

    Fixed messages in user login

--- frontend/src/app/user-login/user-login.component.ts
+++ frontend/src/app/user-login/user-login.component.ts
@@ -10,9 +10,7 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css'],
-  providers:[AuthService]
-
+  styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
 
@@ -21,8 +19,7 @@ export class UserLoginComponent implements OnInit {
   infoMessage = '';
   userToken: string;
   loggedIn = false;
-  loggedIn$ = false;
-
+  loggedInSubject = new BehaviorSubject<boolean>(!!localStorage.getItem('userToken'));
 
   secureEndpointResponse = '';
   hide= true;
@@ -30,20 +27,23 @@ export class UserLoginComponent implements OnInit {
   constructor(private httpClient: HttpClient,
               private authService: AuthService,
               private route: ActivatedRoute,
-              public snackBar: MatSnackBar) {
+              public snackBar: MatSnackBar) { }
 
-    //Subscribes to the loggIn$ observable
-    authService.loggedIn$.subscribe((nextValue) => {
-      this.loggedIn$ = nextValue;  // this will happen on every change
-    })
+  ngOnInit() {
+    this.checkUserStatus()
   }
 
+  /**
+   * Displays a message at the bottom of the screen
+   *
+   * @param message, a string with the message to deisplay
+   * @param action
+   */
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
       duration: 8000,
     });
   }
-
 
   checkUserStatus(): void{
     // Get user data from local storage
@@ -54,62 +54,36 @@ export class UserLoginComponent implements OnInit {
     this.loggedIn = !!(this.userToken);
     this.authService.login = !!(this.userToken);
   }
-  async login(): Promise<void> {
-    this.loginRequest();
 
-    this.checkUserStatus();
-
-    //gives the user a message if login was successful not successful
-    if (!this.loggedIn) {
-      this.openSnackBar('Login was not possible, please check username and password', '')
-    }
-
-  }
-
-  loginRequest(): void {
-    this.httpClient.post(environment.endpointURL + 'user/login', {
+   login(): void {
+     this.httpClient.post(environment.endpointURL + 'user/login', {
       userName: this.userName,
       password: this.password
     }).subscribe((res: any) => {
       // Set user data in local storage
       localStorage.setItem('userToken', res.token);
       localStorage.setItem('userName', res.user.userName);
-      this.authService.login = true;
-      this.loggedIn = true;
 
-      //gives the user a message if login was successful
-      this.openSnackBar('Login was successful', '')
-    });
+      this.checkUserStatus();
+      this.openSnackBar('You successfully logged in!', '')
+    }, (error: any) => {
+       this.checkUserStatus();
+       this.openSnackBar('Login was not successful, please check username and password', '')
+     });
+
   }
 
 
-ngOnInit() {
-  this.checkUserStatus();
-  this.route.queryParams
-      .subscribe(params => {
-        if(params.registered === 'true') {
-            this.infoMessage = 'Registration Successful! Please Login!';
-        }
-      });
-}
-
-
+  /**
+   * Loges the user out
+   */
   logout(): void {
     // Remove user data from local storage
     localStorage.removeItem('userToken');
     localStorage.removeItem('userName');
 
-    this.authService.login = false;
-    //this.loggedIn = false;
-
     this.checkUserStatus();
-    //gives the user a message if logout was successful
-    if (!this.loggedIn) {
-      this.openSnackBar('You successfully logged out!', '')
-    }
-    else {
-      this.openSnackBar('Something went wrong,! You are still logged in', '')
-    }
+    this.openSnackBar('You successfully logged out!', '')
   }
 
 
