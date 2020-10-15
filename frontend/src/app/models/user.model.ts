28b32ed5d0a57461046c9e5d21239c100178a0e0
Yves Zimmermann <yves.zimmermann1@students.unibe.ch>
2020-10-15 16:23:35 +0200

    Implemented admin role and dashboard

---
+++ frontend/src/app/models/user.model.ts
@@ -0,0 +1,21 @@
+import { TodoItem } from './todo-item.model';
+
+export class User {
+
+  constructor(
+    public userId: number,
+    public userName: string,
+    public password: string,
+    public email: string,
+    public firstName: string,
+    public lastName: string,
+    public gender: string,
+    public telephone: string,
+    public street: string,
+    public pinCode: number,
+    public city: string,
+    public country: string,
+    public moneyInWallet: number,
+    public role: string,
+  ) {}
+}
