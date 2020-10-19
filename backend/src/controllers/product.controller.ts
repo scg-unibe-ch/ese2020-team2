fa2a1b08bb2a74167c7e20eba2fd816389cdb549
HarikaPutta <harika.putta.vsm@gmail.com>
2020-10-19 23:18:52 +0200

    Pushing 'add, delete, edit, get, get all' product features

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,39 +1,80 @@
 import express from 'express';
 import { Router, Request, Response } from 'express';
-import { ProductService } from '../services/product.service';
-import {Product} from '../models/product.model';
+import { Product } from '../models/product.model';
 
 const productController: Router = express.Router();
-const productService = new ProductService();
 
 /**
- * This method is to add new products/services to the products model. This calls the 'addProduct'
- * method from the ProductService.
+ * This method is to add new products/services to the products model.
  */
-
-productController.post('/addProduct',
-(req: Request, res: Response) => {
-    productService.addProduct(req.body)
+productController.post('/add',
+    (req: Request, res: Response) => {
+        Product.create(req.body)
         .then(product_added => res.send(product_added))
         .catch(err => res.status(500).send(err));
-});
+    });
 
 /**
  * This Method enables to delete a product from the list of lend or sell products.
  *
  * @param product
  */
-
-productController.delete('/:id', (req: Request, res: Response) => {
+productController.delete('/delete/:id', (req: Request, res: Response) => {
     Product.findByPk(req.params.id)
         .then(found => {
             if (found != null) {
-                found.destroy().then(() => res.status(200).send('Product removed success'));
+                found.destroy().then(() => res.status(200).send('Product removed successfully.'));
             } else {
-                res.sendStatus(404).send('cannot delete the product');
+                res.sendStatus(404).send('Product not found.');
             }
         })
-        .catch(err => res.status(500).send('cannot find the product'));
+        .catch(err => res.status(500).send(err));
 });
 
+/**
+ * This method is to edit a product in the products model.
+ */
+productController.put('/edit/:id', (req: Request, res: Response) => {
+    Product.findByPk(req.params.id)
+        .then(found => {
+            if (found != null) {
+                found.update(req.body).then(() => {
+                    res.status(200).send('Product updated successfully.');
+                });
+            } else {
+                res.sendStatus(404).send('Product not found.');
+            }
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+/**
+ * This method is to get the list of all products in the products model.
+ */
+productController.get('/getAll',
+    (req: Request, res: Response) => {
+        Product.findAll()
+            .then(list => res.status(200).send(list))
+            .catch(err => res.status(500).send(err));
+    });
+
+/**
+ * This method is to get a specific product from the products model.
+ */
+productController.get('/get/:id',
+    (req: Request, res: Response) => {
+        Product.findByPk(req.params.id)
+            .then(found => {
+                if (found != null) {
+                    Product.findOne({
+                        where: { productId: req.params.id }
+                    })
+                        .then(product => res.status(200).send(product))
+                        .catch(err => res.status(500).send(err));
+                } else {
+                    res.sendStatus(404).send('Product not found.');
+                }
+            })
+            .catch(err => res.status(500).send(err));
+    });
 export const ProductController: Router = productController;
