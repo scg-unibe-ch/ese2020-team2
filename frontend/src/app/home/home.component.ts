976f225388aa3d2dc3cf097bd2de20835c181c63
Blendi Shala <blendi.shala@students.unibe.ch>
2020-10-26 13:50:08 +0100

    Made the home as the "wiki" of the project
    There is a guide on the bottons of the website, which tells the user what the buttons do

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from "./../auth/auth.service";
+import {Role} from "./../models/role";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  loggedIn$=false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    //Subscribes to the loggIn$ observable
+    authService.loggedIn$.subscribe((nextValue) => {
+      this.loggedIn$ = nextValue;  // this will happen on every change
+    })
   }
 
+   /**
+   * Checks if user is logged in and updates the login status of the user
+   */
+  ngOnInit() {
+    this.authService.login = !!(localStorage.getItem('userToken'));
+  }
+  get isAdmin() {
+    return this.authService.hasRole(Role.Admin);
+  }
 }
