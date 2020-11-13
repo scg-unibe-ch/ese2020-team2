dcfce0b34fbfb29289ce46950ce44bbec9977ab9
Yves Zimmermann <yves.zimmermann1@students.unibe.ch>
2020-11-13 18:00:18 +0100

    Implemented the purchase information in the shopping cart

--- frontend/src/app/user-account/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/user-account/shopping-cart/shopping-cart.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {ProductsService} from "../../services/products.service";
 import {Observable} from "rxjs";
 import {Product} from "../../models/product.model";
+import {CurrentUser} from "../../services/current-user";
 
 @Component({
   selector: 'app-shopping-cart',
@@ -12,19 +13,24 @@ import {Product} from "../../models/product.model";
 export class ShoppingCartComponent implements OnInit {
   private shoppingCart = [2,1,3,4,5];
   shoppingCart$: Observable<Product[]>;
-  private products: Product[];
   quantity = 1;
+  points$: Observable<number>;
 
   constructor(private httpClient: HttpClient,
-              private productsService: ProductsService) { }
+              private productsService: ProductsService,
+              private users: CurrentUser) { }
 
   ngOnInit(): void {
     this.getShoppingCart();
+    this.points$ = this.users.getCurrentUserProperty('moneyInWallet');
+
   }
 
   getShoppingCart(): void {
     this.shoppingCart$ = this.productsService.getProductsByMultipleIds(this.shoppingCart);
   }
+
+
 
 
 }
