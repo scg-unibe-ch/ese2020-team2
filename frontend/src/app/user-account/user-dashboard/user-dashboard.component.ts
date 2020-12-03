51fbd8911369b52efcc1219d0bd5608301b66a8b
Blendi Shala <blendi.shala@students.unibe.ch>
2020-12-03 22:29:00 +0100

    Added the routing in the user account
    Now it is possible to see the routing in the url

--- frontend/src/app/user-account/user-dashboard/user-dashboard.component.ts
+++ frontend/src/app/user-account/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  title = 'angular-material-tab-router';  
+  navLinks: any[];
+  activeLinkIndex = -1;
+  constructor(private router:Router) {
+    this.navLinks = [
+      {
+          label: 'Posted Items',
+          link: './posted',
+          index: 0
+      }, {
+          label: 'Sold Items',
+          link: './sold',
+          index: 1
+      }, {
+          label: 'Lent Items',
+          link: './lent',
+          index: 2
+      }, {
+        label: 'Purchased History',
+        link: './purchasedHistory',
+        index: 3
+    },
+  ];
+   }
 
   ngOnInit(): void {
+    this.updateIndex()
   }
-
+updateIndex(){
+  this.router.events.subscribe((res) => {
+    this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+});
+}
 }
