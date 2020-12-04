514f08061fe1d62d97f829baccc248ae61b710b7
HarikaPutta <harika.putta.vsm@gmail.com>
2020-12-04 12:40:01 +0100

    Adding the product image functionality

--- backend/src/controllers/productImage.controller.ts
+++ backend/src/controllers/productImage.controller.ts
@@ -1,131 +1,86 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
-import {create} from 'domain';
 import multer from 'multer';
-import { User } from '../models/user.model';
-
-import { verifyToken } from '../middlewares/checkAuth';
-import { imageService } from '../services/productImage.service';
-import path from 'path';
-import { ProductImage } from '../models/productImage.model';
+import fs from 'fs';
 import { Product } from '../models/product.model';
-
-
-
-const config = Object.freeze({uploadImagePath: process.env.UPLOAD_PATH || './uploads'});
-
-const imageStorage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function(req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
-    }
-});
-
-/**
- * Here are filtered the image we want to accept as upload for product images
- * @param req
- * @param file will be set as any and defined directly in the constructor
- * @param cb will throw a boolean and error message if the picture format is wrong
- */
-const imageFilter = (req: any, file: any, cb: (arg0: any, arg1: boolean) => void) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        cb(new Error('Wrong file format, please choose .png or .jpeg files'), false);
-    }
-
-};
-const uploadImages = multer({
-    storage: imageStorage,
-    limits: {
-    fileSize: 1024 * 1024 * 5 // limit to 5 MB picture
-    },
-    fileFilter: imageFilter
-});
+import { ProductImage } from '../models/productImage.model';
 
 const imageController = express.Router();
 
+// Defining the storage location for the images and renaming the file with uniqe name.
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, getDateTime() + file.originalname);
+    }
+});
+
+// Checks for the type of the file uploaded. If non jpeg or png files are uploaded it throws an error.
+const fileFilter = (req: any, file: any, cb: (arg0: any, arg1: boolean) => void) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(new Error('Choose a valid file type'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    // Limits the size of the images to 5MB.
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+// Returns a string with current date and time without special characters.
+function getDateTime() {
+    const date = new Date().toISOString();
+    console.log(date);
+    return date.replace(/[^a-zA-Z0-9]/g, '');
+}
+
+// Adds a new image to the uploads folder and creates a record in ProductImage table.
 imageController.post(
-    '/:productId',
-  //  verifyToken,
-    uploadImages.array('images', 5),
-    // uploadImages.array('images[]', 5),
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const files = req.files as any[];
-
-            const uploadPromises = files.map( file => {
-                const image = {
-                    fileId: file.fileId,
-                    productId: req.params.productId,
-                    userId: req.body.userId,
-                    fileName: file.fileName
-                };
-                return imageService.create(image);
-            });
-            await Promise.all(uploadPromises);
-            res.send({ success: true });
-        } catch (err) {
-            return next(err);
+    '/add/', upload.single('productImage'), async (req: Request, res: Response, next: NextFunction) => {
+        console.log(req.protocol + '://' + req.get('host'));
+        req.body.filePath = req.protocol + '://' + req.get('host') + (req.file.destination).substring(1) + req.file.filename;
+        if (Product.findOne({ where: { productId: req.body.productId, userId: req.body.userId } })) {
+            ProductImage.create(req.body).then(image_added => res.send(image_added)).catch(err => res.status(500).send(err));
+        } else {
+            res.status(404).send('No such product or user found.');
         }
-    }
-);
-
-imageController.get(
-    '/:fileName',
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const fileName = req.params.fileName;
-            res.sendFile(path.join(process.cwd(), config.uploadImagePath + `/${fileName}`));
-        } catch (err) {
-            next(err);
-        }
-    }
-);
-
-
-
-
-
-
-
-/*
-imageController.post('/addFile', uploadImages.single('uploadedImage'),
-    (req, res, next) => {
-        console.log(req.file);
-
-        const product = new Product({
-            id: Product.userId(),
-            name: req.body.name,
-            price: req.body.price,}
-        )
-        if (!file) {
-            const error = new Error('Please upload a file')
-            err.httpStatusCode = 400
-            return next(error)
-        }
-        res.status(200).send({
-            statusCode: 200,
-            status: 'success',
-            uploadedFile: file
-        })
-
-    }, (error, req, res, next) => {
-        res.status(400).send({
-            error: error.statusMessage // To be checked
-        })
     });
- */
-/*
-imageController.post('/image',
-    uploadImages.single('productImage'),
-    (req, res, next) => {
-        console.log(req.file);
-        //const product = new Product()
-        Product.create(req.body)
-            .then(product_added => res.send(product_added))
+
+// Returns all the image URL's for the given product.
+imageController.get(
+    '/get/:productId', async (req: Request, res: Response) => {
+        ProductImage.findAll({ where: { productId: req.params.productId }, attributes: ['filePath'] })
+            .then(found => res.send(found))
             .catch(err => res.status(500).send(err));
     });
-*/
+
+// Deletes an image from the ProductImage table and the uploads folder.
+imageController.delete(
+    '/delete/:id', async (req: Request, res: Response) => {
+        ProductImage.findByPk(req.params.id)
+            .then(found => {
+                if (found != null ) {
+                    found.destroy().then(() => {
+                        const fileName = found.filePath.substring(found.filePath.lastIndexOf('/') + 1);
+                        try {
+                        fs.unlinkSync('./uploads/' + fileName);
+                        } catch {
+                            res.status(500).send('Image does not exist in the directory');
+                        }
+                        res.status(200).send('Image deleted');
+                    });
+                } else {
+                    res.status(404).send('Image not found');
+                }
+            })
+            .catch(err => res.status(500).send(err));
+    });
+
 export const ImageController: Router = imageController;
