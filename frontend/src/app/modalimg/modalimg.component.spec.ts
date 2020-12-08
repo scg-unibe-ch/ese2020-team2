f62979233c2d42e5244aed7db856bfab09c66541
Blendi Shala <blendi.shala@students.unibe.ch>
2020-12-08 14:34:37 +0100

    Fixing some tests

--- frontend/src/app/modalimg/modalimg.component.spec.ts
+++ frontend/src/app/modalimg/modalimg.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 import { ModalimgComponent } from './modalimg.component';
 
 describe('ModalimgComponent', () => {
   let component: ModalimgComponent;
   let fixture: ComponentFixture<ModalimgComponent>;
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModalimgComponent ]
+      declarations: [ ModalimgComponent ],
+      imports:[MatDialogModule],
+      providers:[{provide: MatDialogRef, useValue:mockDialogRef}],
     })
     .compileComponents();
   }));
