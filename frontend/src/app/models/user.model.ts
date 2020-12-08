a2d3be0e16802727f755f35c5f73b4e915b528bc
Yves Zimmermann <yves.zimmermann1@students.unibe.ch>
2020-12-08 23:07:59 +0100

    Finished commenting

--- frontend/src/app/models/user.model.ts
+++ frontend/src/app/models/user.model.ts
@@ -1,18 +1,31 @@
 export class User {
 
   constructor(
+    // The Id uf the user
     public userId: number,
+    // The user name
     public userName: string,
+    // The password
     public password: string,
+    // The email
     public email: string,
+    // The first name
     public firstName: string,
+    // The last name
     public lastName: string,
+    // The gender
     public gender: string,
+    // The street
     public street: string,
+    // The pin code
     public pinCode: string,
+    // The city
     public city: string,
+    // The country
     public country: string,
+    // The money the user possesses
     public moneyInWallet: number,
+    // The role the user has, either user or admin
     public role: string,
   ) {}
 }
